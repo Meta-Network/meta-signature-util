a533ecc5a8aa6dde74f3fcdcaee90045596551d6
Casimir Crystal <casimir.crystal.blue@outlook.com>
2022-02-18 13:22:48 +0800

    feat: throw an error if the signature is invalid

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -89,40 +89,86 @@ describe('test generateDigest', () => {
   test('the digest in result should be sha256 hash', () => {
     expect(digestMetadata.digest).toMatch(/0x\w{64}/);
   });
+
   test('the digest in result should be the same as the excepted', () => {
     expect(digestMetadata.digest).toBe(exceptedDigest);
+  });
+
+  test('test generateDigest with empty payload values', () => {
+    // except this calling to be successful
+    expect(
+      authorPostDigest.generate({
+        title: '',
+        categories: '',
+        content: '',
+        cover: '',
+        license: '',
+        summary: '',
+        tags: '',
+      }),
+    ).toEqual({
+      '@context': 'https://metanetwork.online/ns/cms',
+      '@type': 'author-post-digest',
+      '@version': '1.1.0',
+      algorithm: 'sha256',
+      categories: '',
+      content: '',
+      cover: '',
+      digest:
+        '0x4c81c93371b8f17b79dcecbd9ebd5105a239e9da7906778c3e0debe81d38388c',
+      license: '',
+      summary: '',
+      tags: '',
+      title: '',
+      ts: 1636198657477,
+    });
   });
 });
 
 describe('test generateSignature', () => {
-  authorSignatureMetadata = authorPostDigestSign.generate(
-    keys,
-    'metaspace.life',
-    digestMetadata.digest,
-  );
-  const exceptedSignature =
-    '0x9ab3c80768805e7842a4c1733d47e007a35bdbcdb9a5899ed5c9df6fa353cc9ddf65e9675700e921c2aa70f895c77f3b13aa119811fccbe0cbf684c40b37d880';
-
   test('the signature in result should be the same as the excepted', () => {
+    authorSignatureMetadata = authorPostDigestSign.generate(
+      keys,
+      'metaspace.life',
+      digestMetadata.digest,
+    );
+    const exceptedSignature =
+      '0x9ab3c80768805e7842a4c1733d47e007a35bdbcdb9a5899ed5c9df6fa353cc9ddf65e9675700e921c2aa70f895c77f3b13aa119811fccbe0cbf684c40b37d880';
+
     expect(authorSignatureMetadata.signature).toBe(exceptedSignature);
   });
 });
 
 describe('test verifyDigest', () => {
-  const verifiedResult = authorPostDigest.verify(digestMetadata);
   test('verify digest result should be true if the metadata is unchanged', () => {
+    const verifiedResult = authorPostDigest.verify(digestMetadata);
     expect(verifiedResult).toBe(true);
   });
 
   const digestMetadataCopy = { ...digestMetadata };
   digestMetadataCopy.title = 'The title has been changed';
   const notVerifiedResult = authorPostDigest.verify(digestMetadataCopy);
-  test('verify digest result should be false if the metadata was changed', () => {
+
+  test('verify digest result should be false if the metadata is changed', () => {
     expect(notVerifiedResult).toBe(false);
+  });
+
+  test('verify digest result should be false if the digest is not match or empty', () => {
+    const emptyDigestMetadata = {
+      ...digestMetadata,
+      digest: '',
+    };
+    expect(authorPostDigest.verify(emptyDigestMetadata)).toBe(false);
   });
 });
 
 describe('test verifySignature', () => {
+  authorSignatureMetadata = authorPostDigestSign.generate(
+    keys,
+    'metaspace.life',
+    digestMetadata.digest,
+  );
+
   const verifiedResult = authorPostDigestSign.verify(authorSignatureMetadata);
   test('verify signature result should be true if the public key is correct', () => {
     expect(verifiedResult).toBe(true);
@@ -135,5 +181,14 @@ describe('test verifySignature', () => {
   );
   test('verify signature result should be false if the metadata was changed', () => {
     expect(verifiedResultSecond).toBe(false);
+  });
+  test('should throw "The signature is not a valid hex string" when the signature is invalid', () => {
+    const emptySignatureMetadata = {
+      ...authorSignatureMetadata,
+      signature: '',
+    };
+    expect(() => authorPostDigestSign.verify(emptySignatureMetadata)).toThrow(
+      new TypeError('The signature is not a valid hex string'),
+    );
   });
 });
